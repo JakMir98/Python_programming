302eda0b655dab13061aae9273f2101ede0bd582
Jakub Mirota <47855541+JakMir98@users.noreply.github.com>
2020-12-14 17:43:14 +0100

    Delete we:wy2.py

--- we:wy2.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python
-var = str(
-    input("Enter name surname and year of birth\n please use spaces beetwen all"))
-var2 = var.split(" ")
-print("name: " + var2[0] + "\nsurname: " + var2[1] + "\ndate : " + var2[2])
